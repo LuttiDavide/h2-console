8d2ba0ae50d30a63c086fca721489b20f3fb3d4f
Lutti Davide <luttidavide004@gmail.com>
2024-04-22 16:46:22 +0200

    first commit

---
+++ src/main/java/co/develhope/h2mock/entities/Student.java
@@ -0,0 +1,23 @@
+package co.develhope.h2mock.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "students")
+@Data
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+}
